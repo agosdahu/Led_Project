4e612bb365c5709ba05d6910292008b9aa37af78
nagyandrea <50954333+nagyandrea@users.noreply.github.com>
2019-05-26 21:47:45 +0200

    Add files via upload
    Chasing lights feature is added.

---
+++ ledProject/chasingLights.h
@@ -0,0 +1,11 @@
+#ifndef CHASINGLIGHTS_H
+#define CHASINGLIGHTS_H
+
+#include "switch_handler.h"
+#include <Arduino.h>
+
+void ChasingLights_OneByOne( );
+void ChasingLights_TwoByTwo( );
+void ChasingLights( );
+
+#endif /* CHASINGLIGHTS_H */

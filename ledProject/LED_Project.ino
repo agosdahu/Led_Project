51dd1db04827219a8e05ee79771680fe27ec03bb
Bogdan Kurdi <bogdan.kurdi@bbraun.com>
2019-05-27 10:01:12 +0200

    kurdbohu feature added
    initial

---
+++ ledProject/LED_Project.ino
@@ -0,0 +1,67 @@
+extern "C"{
+   #include "led_handler.h"
+   #include "switch_handler.h"
+   #include "sos_led.h"
+   #include "binary_counter.h"
+   #include "chasingLights.h"
+   #include "LightRace.h"
+   #include "railBarrier.h"
+   #include "kurdbohu.h"
+}
+
+ SW_STATUS_t sw = {RELEASED, RELEASED};
+  
+void setup() {
+  
+  LedInit();
+  SwitchInit();
+}
+
+void loop() {
+  
+  static int EN = 0;
+  
+  
+  sw = ReadSwitchStatus();
+
+  if(sw.sw0 == PRESSED)
+  {
+    EN = 1;
+  }
+
+  if(sw.sw1 == PRESSED)
+  {
+    EN = 0;
+  }
+
+  if(EN)
+  {
+#ifdef AGOSDAHU
+    sos_ledBlink();
+#endif
+
+#ifdef VEZSBAHU
+#endif
+
+#ifdef KURDBOHU
+	kurdbohu_loop();
+#endif
+
+#ifdef DANIGEHU
+     /*TODO: uncomment when LightRace merge is complete. */
+     /* LightRace( 20000 ); */
+#endif
+
+#ifdef NAGYKAHU
+    binary_counter();
+#endif
+
+#ifdef NAGYARHU
+    ChasingLights();
+#endif
+
+#ifdef KISSKAHU
+   railBarrier();
+#endif
+  }
+}
---
+++ ledProject/kurdbohu.h
@@ -0,0 +1,12 @@
+/*
+    author  : kurdbohu
+    date    : 2019-05-26
+*/
+
+#ifndef KURDBOHU_H
+  #define KURDBOHU_H
+  
+  // global function
+  void kurdbohu_loop(void);
+
+#endif // KURDBOHU_H

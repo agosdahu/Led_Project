fa083de5a4152ddd649fd9555459f12b4898535d
danigeri <danigerci@gmail.com>
2019-05-27 10:42:37 +0200

    Update globdefs.h
    Updated led pins for customer device

--- ledProject/globdefs.h
+++ ledProject/globdefs.h
@@ -3,12 +3,12 @@
 
 #define LED0        2u
 #define LED1        3u
-#define LED2        4u
+#define LED2        8u
 #define LED3        5u
-#define LED4        6u
-#define LED5        7u
-#define SWITCH0     8u
-#define SWITCH1     9u
+#define LED4        7u
+#define LED5        10u
+#define SWITCH0     11u
+#define SWITCH1     12u
 
 #define AGOSDAHU
 //#define VEZSBAHU

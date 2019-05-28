c0d3e06f131aad90142de2742081414ab4226ed0
kurdbohu <50950226+kurdbohu@users.noreply.github.com>
2019-05-28 23:59:07 +0200

    Led Handler refactoring
    Please test it on hardware before reviewing the code!
    -----------------------------------------------------------
    Modifications:
    - Separated high level & low level.
    - Added global functions:
       -- void SetLed(uint8_t pin);
       -- void ClearLed(uint8_t pin);
       -- void SwitchLedState(uint8_t pin);

--- ledProject/led_handler.c
+++ ledProject/led_handler.c
@@ -1,18 +1,171 @@
 #include "led_handler.h"
 
+// --------------------------------------------------------- //
+// local definitions
+
+#define NUM_OF_LEDS  6
+
+// --------------------------------------------------------- //
+// local structured tyes
+
+typedef struct led_state
+{
+    uint8_t pin;
+    uint8_t state;
+}LED_STATE;
+
+
+// --------------------------------------------------------- //
+// static functions
+
+static int8_t  GetLedState(uint8_t pin);
+static void    SetLedState(uint8_t pin, uint8_t state);
+static int8_t  GetLedIndex(uint8_t pin);
+
+static void HW_SetLed(uint8_t pin);
+static void HW_ClearLed(uint8_t pin);
+static void HW_SetPinInput(uint8_t pin);
+static void HW_SetPinOutput(uint8_t pin);
+
+// --------------------------------------------------------- //
+// static variables
+
+static LED_STATE LedStates[NUM_OF_LEDS] =
+{
+    {LED0, LOW},
+    {LED1, LOW},
+    {LED2, LOW},
+    {LED3, LOW},
+    {LED4, LOW},
+    {LED5, LOW},
+};
+
+
+// --------------------------------------------------------- //
+// implementation
+
 void LedInit(void)
 {
-    pinMode(LED0, OUTPUT);
-    pinMode(LED1, OUTPUT);
-    pinMode(LED2, OUTPUT);
-    pinMode(LED3, OUTPUT);
-    pinMode(LED4, OUTPUT);
-    pinMode(LED5, OUTPUT);
+    HW_SetPinOutput(LED0);
+    HW_SetPinOutput(LED1);
+    HW_SetPinOutput(LED2);
+    HW_SetPinOutput(LED3);
+    HW_SetPinOutput(LED4);
+    HW_SetPinOutput(LED5);
 
-    digitalWrite(LED0, LOW);
-    digitalWrite(LED1, LOW);
-    digitalWrite(LED2, LOW);
-    digitalWrite(LED3, LOW);
-    digitalWrite(LED4, LOW);
-    digitalWrite(LED5, LOW);
+    ClearLed(LED0);
+    ClearLed(LED1);
+    ClearLed(LED2);
+    ClearLed(LED3);
+    ClearLed(LED4);
+    ClearLed(LED5);
 }
+
+void SetLed(uint8_t pin)
+{
+    SetLedState(pin, HIGH);  
+}
+
+void ClearLed(uint8_t pin)
+{
+    SetLedState(pin, LOW);
+}
+
+void SwitchLedState(uint8_t pin)
+{
+    int8_t  LedIndex = -1;
+    int8_t  LedState = -1;
+
+    LedIndex = GetLedIndex(pin);
+
+    if (LedIndex != -1)
+    {
+        LedState = LedStates[LedIndex].state;
+        if (LedState == LOW)
+        {
+            SetLed(pin);
+        }
+        else
+        {
+            ClearLed(pin);
+        }
+    }
+}
+
+static int8_t GetLedState(uint8_t pin)
+{
+    int8_t  LedIndex = -1;
+    int8_t  LedState = -1;
+
+    LedIndex = GetLedIndex(pin);
+
+    if (LedIndex != -1)
+    {
+        LedState = LedStates[LedIndex].state;
+    }
+
+    return (LedState);
+}
+
+static void SetLedState(uint8_t pin, uint8_t state)
+{
+    int8_t LedIndex = -1;
+
+    LedIndex = GetLedIndex(pin);
+
+    if (LedIndex != -1)
+    {
+        LedStates[LedIndex].state = state;
+
+        if (state == LOW)
+        {
+            HW_ClearLed(pin);
+        }
+        else
+        {
+            HW_SetLed(pin);
+        }
+        
+    }
+}
+
+static int8_t GetLedIndex(uint8_t pin)
+{
+    int8_t  LedIndex = -1;
+    uint8_t i;
+    
+    for(i = 0; i < NUM_OF_LEDS; i++)
+    {
+        if (LedStates[i].pin == pin)
+        {
+            LedIndex = i;
+            break;
+        }
+    }
+
+    return(LedIndex);
+}
+
+// --------------------------------------------------------- //
+// low level functions
+
+static void HW_SetLed(uint8_t pin)
+{
+    digitalWrite(pin, HIGH);
+}
+
+static void HW_ClearLed(uint8_t pin)
+{
+    digitalWrite(pin, LOW);
+}
+
+static void HW_SetPinInput(uint8_t pin)
+{
+    pinMode(pin, INPUT);
+}
+
+static void HW_SetPinOutput(uint8_t pin)
+{
+    pinMode(pin, OUTPUT);
+}
+
--- ledProject/led_handler.h
+++ ledProject/led_handler.h
@@ -5,5 +5,8 @@
 #include "globdefs.h"
 
 void LedInit(void);
+void SetLed(uint8_t pin);
+void ClearLed(uint8_t pin);
+void SwitchLedState(uint8_t pin);
 
 #endif //LED_HANDLER_H

98c1596e4c856da3be5bf1a28774f74667637d9e
David Agoston <david.agoston@bbraun.com>
2019-06-04 11:23:19 +0200

    Initial commit

---
+++ ledProject/driver.h
@@ -0,0 +1,8 @@
+#ifndef DRIVER_H
+#define DRIVER_H
+
+    #include "Arduino.h"
+    #include "led_handler.h"
+    #include "switch_handler.h"
+
+#endif //DRIVER_H
--- ledProject/led_handler.h
+++ ledProject/led_handler.h
@@ -1,9 +1,16 @@
 #ifndef LED_HANDLER_H
 #define LED_HANDLER_H
 
-#include "Arduino.h"
-#include "globdefs.h"
+#include "driver.h"
 
-void LedInit(void);
+// global functions
+void   LedInit(void);
+void   SetLed(uint8_t pin);
+void   ClearLed(uint8_t pin);
+int8_t GetLedState(uint8_t pin);
+void   SetLedState(uint8_t pin, uint8_t state);
+void   SwitchLedState(uint8_t pin);
+
+
 
 #endif //LED_HANDLER_H

8d04a3762fa6bc37f59e66890d8e023795cfe0d0
David Agoston <david.agoston@bbraun.com>
2019-06-04 17:43:26 +0200

    millis function implementation
    GetDiffTime returns the elapsed time in milliseconds since the last reading

--- ledProject/customDelay.h
+++ ledProject/customDelay.h
@@ -1,7 +1,7 @@
 #ifndef CUSTOMDELAY_H
 #define CUSTOMDELAY_H
 
-void DelayMillis(int millis);
-int  DiffTime (int lastTime);
+void DelayMillis(int millisec);
+int  GetDiffTime (void);
 
 #endif // CUSTOMDELAY_H

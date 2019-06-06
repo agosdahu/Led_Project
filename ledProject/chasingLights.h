c44f4101140192bd4b17c94ccc9c19c8c6986003
nagyandrea <50954333+nagyandrea@users.noreply.github.com>
2019-06-06 11:10:32 +0200

    Update chasingLights.h

--- ledProject/chasingLights.h
+++ ledProject/chasingLights.h
@@ -1,8 +1,7 @@
 #ifndef CHASINGLIGHTS_H
 #define CHASINGLIGHTS_H
 
-#include "switch_handler.h"
-#include <Arduino.h>
+#include "driver.h"
 
 void ChasingLights_OneByOne( );
 void ChasingLights_TwoByTwo( );
